9f690342925a99032dd4a04baa56b8a1f50acbbd
olivia <gr982220@ohio.edu>
2022-09-07 17:36:34 -0400

    created the program, compiled, and ran it

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Olivia Radecki
+ *        Date: 9/7/2002
+ * Description: Lab 2 program
+ */
+ 
+ #include <iostream>
+ #include <iomanip>
+ #include <cstdlib>
+ using namespace std;
+
+
+ int main(int argc, char const *argv[]){
+     cout << "Hello Git!!" << endl;
+     return 0;
+ }
